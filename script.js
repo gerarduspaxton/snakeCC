664e09b59cab829c3fad7e98a546053a52694d5f
gerarduspaxton <120147511+gerarduspaxton@users.noreply.github.com>
2022-12-12 12:01:04 +0700

    lb fix

--- script.js
+++ script.js
@@ -238,7 +238,7 @@ function updateLeaderboard() {
   if(user == "") {
     user = "Anonymous";
   }
-  gridHeight == 9 && gridWidth == 9 && snakeX.length - 3 > leaderboard[user.toUpperCase()] ? leaderboard[user.toUpperCase()] = snakeX.length - 3 : "";
+  gridHeight == 9 && gridWidth == 9 && (snakeX.length - 3 > leaderboard[user.toUpperCase()] || Object.values(leaderboard).includes(user.toUpperCase()) == false) ? leaderboard[user.toUpperCase()] = snakeX.length - 3 : "";
   window.localStorage.setItem("lb", JSON.stringify(leaderboard));
   orderLeaderboard();
   for(d = 0; d < 5; d++) {
