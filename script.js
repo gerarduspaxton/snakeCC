f889e293417e173a8e2a7da754110569f20e08ff
gerarduspaxton <120147511+gerarduspaxton@users.noreply.github.com>
2022-12-13 08:38:06 +0700

    speed minimum

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function updateLeaderboard() {
   if(user == "") {
     user = "Anonymous";
   }
-  gridHeight == 9 && gridWidth == 9 && (snakeX.length - 3 > leaderboard[user.toUpperCase()] || Object.keys(leaderboard).includes(user.toUpperCase()) == false) ? leaderboard[user.toUpperCase()] = snakeX.length - 3 : "";
+  gridHeight == 9 && gridWidth == 9 && speed >= 8 && (snakeX.length - 3 > leaderboard[user.toUpperCase()] || Object.keys(leaderboard).includes(user.toUpperCase()) == false) ? leaderboard[user.toUpperCase()] = snakeX.length - 3 : "";
   window.localStorage.setItem("lb", JSON.stringify(leaderboard));
   orderLeaderboard();
   for(d = 0; d < 5; d++) {
